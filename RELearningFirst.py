85b8eeca4adeb5b4ade5cb1b3016ca29c978b248
NewmanZhou <fazhuolanyi@163.com>
2018-07-19 13:12:26 +0800

    modfiy: 新增正则花括号 字符组（）

--- RELearningFirst.py
+++ RELearningFirst.py
@@ -2,6 +2,12 @@
 import re
 
 # 正则的归纳整理
+'''
+怎么会爱上了他，还不是因为眼瞎
+放弃了一片森林，被根破草绊倒了
+纸短情长啊，写不完你的鬼话
+一包A4写不完你有多渣
+'''
 
 # 元字符
 '''
@@ -32,14 +38,14 @@ $     匹配行的结束
 '''
 
 # match 从字符串其实位置开始匹配 开头没有符合规则的 就不再匹配 返回None
-m = re.match('.ar','The car parked in the garage.')
+m = re.match('.ar', 'The car parked in the garage.')
 
 # 匹配字符串中第一个 符合规则的 并返回 不再继续匹配
-m = re.search('.ar','The car parked in the garage.')
+m = re.search('.ar', 'The car parked in the garage.')
 print(m.group())
 
 # 匹配所有符合规则的 并返回数组
-m = re.findall('.ar','The car parked in the garage.')
+m = re.findall('.ar', 'The car parked in the garage.')
 print(m)
 
 '''
@@ -83,21 +89,21 @@ print(m)
                        cat            cat
 
 '''
-m = re.findall('[a-z]*','The car parked in the garage #21.')
+m = re.findall('[a-z]*', 'The car parked in the garage #21.')
 print(m)
 
-m = re.findall('\s*cat\s*','The fat cat sat on the cat.')
+m = re.findall('\s*cat\s*', 'The fat cat sat on the cat.')
 print(m)
 
 '''
 1.3.2 加号
 该符号 + 匹配上一个字符的一次或多次。例如正则表达式 c.+t , 
 表示：一个小写字母c,后跟任意数量的字符，后跟小写字母t。
-"c.+t" => The fat cat sat on the mat.
+"c.+t" => The fat cat sat on the mat.       
        =>         cat sat on the mat
 
 '''
-m = re.findall('c.+t','The fat cat sat on the mat.')
+m = re.findall('c.+t', 'The fat cat sat on the mat.')
 print(m)
 
 '''
@@ -107,5 +113,27 @@ print(m)
 "[T]?he" => The car is parked in the garage.
          => The                   he
 '''
-m = re.findall('[T]?he','The car is parked in the garage.')
+m = re.findall('[T]?he', 'The car is parked in the garage.')
 print(m)
+
+'''
+1.4 花括号
+在正则表达式中花括号（也被称为量词？）用于指定字符或一组字符可以重复的次数。例如正则表达式：
+[0-9]{2},表示： 匹配正好为两位数的数字。
+[0-9]{2,3} 表示：匹配至少 2位数字 但不超过 3位数字（0-9范围内的字符）
+"[0-9]{2，3}" => The number was 9.99979 but we rounded it off to 10.0.
+              =>                 999 79                          10
+
+我们可以省略第二个数字。例如正则表达式[0-9]{2,} 表示：匹配2个或更多个数字
+'''
+m = re.findall('[0-9]{2,3}','The number was 9.99979 but we rounded it off to 10.0.')
+print(m)
+
+'''
+1.5 字符组
+字符组是一组写在圆括号内的子模式（...）。正如我们在正则表达式中讨论的那样，如果我们把一个量词放在一个字符之后，他会重复前一个字符，
+但是，如果我们把量词放在一个字符组之后它会重复整个字符组。例如正则表达式（ab）* 表示匹配零个或多个字符串'ab'.
+我们还可以在字符组中使用与字符 | 。例如： （c|g|p）ar 
+"(c|g|p)ar  => The car is parked in the garage"
+            =>     car    par           gar
+'''
