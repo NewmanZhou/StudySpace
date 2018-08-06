31076bc357927728f79c77e8ea02f8eed3d5cf87
NewmanZhou <fazhuolanyi@163.com>
2018-08-06 23:30:31 +0800

    modify: 新增lambda 样例

--- Lambda/LambdaLearning.py
+++ Lambda/LambdaLearning.py
@@ -27,3 +27,22 @@ List.sort(lambda p1, p2: -cmp(p1.age, p2.age))
 print '/n After descending sort'
 for p in List:
     print p.toString()
+
+# Lambda 基本使用
+func = lambda x: x + 1
+print(func(1))
+
+
+# 等同于：
+def func(x):
+    return (x + 1)
+
+
+foo = [1, 2, 4, 5, 7, 8, 9, 18]
+print (list(filter(lambda x: x % 3 == 0, foo)))
+print [x for x in foo if x % 3 == 0]
+
+print (list(map(lambda x: x * 2 + 10, foo)))
+print ([x * 2 + 10 for x in foo])
+
+print (reduce(lambda x, y: x + y, foo))
